dc63636700960727be4d18024e6f20d4f34e4f15
Matthew Johnson <MatthewRonaldJohnson@gmail.com>
2021-04-27 07:45:43 -0500

    added copy right and realigned footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,13 +3,22 @@ import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
     return (
-        <footer className="pt-4 my-md-5 pt-md-5 border-top">
-            <a href="https://github.com/MatthewRonaldJohnson" target="__blank">
-                <FontAwesomeIcon icon={faGithubSquare} size="6x" />
-            </a>
-            <a href="https://www.linkedin.com/in/matthewronaldjohnson/" target="__blank">
-                <FontAwesomeIcon icon={faLinkedin} size="6x" />
-            </a>
+        <footer className="pt-4 my-md-5 pt-md-5 border-top container-fluid">
+            <div className="text-center">
+                © 2021
+            </div>
+            <div className="row">
+                <div className="col text-center">
+                    <a href="https://github.com/MatthewRonaldJohnson" target="__blank">
+                        <FontAwesomeIcon icon={faGithubSquare} size="4x" />
+                    </a>
+                </div>
+                <div className="col text-center">
+                    <a href="https://www.linkedin.com/in/matthewronaldjohnson/" target="__blank">
+                        <FontAwesomeIcon icon={faLinkedin} size="4x" />
+                    </a>
+                </div>
+            </div>
         </footer>
     )
 }
