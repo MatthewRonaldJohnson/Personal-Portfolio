4c8318564721df1791b6799c0c35904ee59ade1d
Matthew Johnson <MatthewRonaldJohnson@gmail.com>
2021-04-27 12:21:26 -0500

    filled out project card component and update project.json file

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,8 +1,21 @@
-export default function ProjectCard(props){
+export default function ProjectCard(props) {
     console.log(props)
     return (
-        <div>
-            card
+        <div className="card mb-3">
+            <div className="row no-gutters">
+                <div className="col-md-4">
+                    <img src={props.img} className="card-img" alt="..." />
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title">{props.title}</h5>
+                        <p className="card-text">{props.writeUp}</p>
+                        <a href={props.deployed} target="__blank" className="card-text">Link to Deployed Application</a>
+                        <br/>
+                        <a href={props.github} target="__blank" className="card-text">Link to Github Repo</a>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
