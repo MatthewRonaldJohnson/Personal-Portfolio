b46af51d5cfa0f7e085b16abca9ec4662b839c9b
Matthew Johnson <MatthewRonaldJohnson@gmail.com>
2021-04-28 15:58:00 -0500

    updated theme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import "../node_modules/bootswatch/dist/journal/bootstrap.min.css"
+import "../node_modules/bootswatch/dist/flatly/bootstrap.min.css"
 
 ReactDOM.render(<App />, document.getElementById('root'));
