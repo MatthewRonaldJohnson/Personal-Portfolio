481b1d104a9a9b06bd562ca7ea4afaf769528a36
Matthew Johnson <MatthewRonaldJohnson@gmail.com>
2021-05-12 09:49:29 -0500

    updated projects.json & resume

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
 import "../node_modules/bootswatch/dist/flatly/bootstrap.min.css"
 
 ReactDOM.render(<App />, document.getElementById('root'));
